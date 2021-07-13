941bae5bfa0d3e29b3174040d84a25049522f2f2
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-07-13 10:43:37 +0900

    BucketList 뷰 만들기

--- bucket_list/src/App.js
+++ bucket_list/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import BucketList from "./BucketList";
 
 class App extends React.Component{
   constructor(props){
@@ -12,11 +13,34 @@ class App extends React.Component{
   render() {
     return (
       <div className="App">
-        <div>
-          <h1>내 버킷리스트</h1>
-          <hr/>
-        </div>
+        <Container>
+          <Title>내 버킷리스트</Title>
+          <Line/>
+          <BucketList list={this.state.list} />
+        </Container>
       </div>
     )
   }
 }
+
+const Container = styled.div`
+  max-width: 350px;
+  min-height: 80vh;
+  background-color: #fff;
+  padding: 16px;
+  margin: 20px auto;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+`;
+
+const Title = styled.h1`
+  color: slateblue;
+  text-align: center;
+`;
+
+const Line = styled.hr`
+  margin: 16px 0px;
+  border: 1px dotted #ddd;
+`;
+
+export default App;
---
+++ bucket_list/src/BucketList.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+const BucketList = (props) => {
+    const my_lists = props.list;
+
+    return(
+        <ListStyle>
+            {my_lists.map((list, index) => {
+                return(
+                    <ItemStyle key={index}>
+                        {list}
+                    </ItemStyle>
+                )
+            })}
+        </ListStyle>
+    )
+}
+
+const ListStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    overflow-x: hidden; // 내용이 잘림, 스크롤바가 나타나지 x
+    overflow-y: auto;   // 내용이 잘림, 필요할 때만 스크롤 나타나
+       
+`;
+
+const ItemStyle = styled.div`
+    padding: 16px;
+    margin: 8px;
+    background-color: aliceblue;
+`;
+
+export default BucketList;
