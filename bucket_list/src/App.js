937b6829d8ac0ec18930e2c653b6c35386c2e4ea
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-07-13 10:25:56 +0900

    뷰 만들기

--- bucket_list/src/App.js
+++ bucket_list/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      list: ["영화관 가기", "매일 책읽기", "수영 배우기"],
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <h1>내 버킷리스트</h1>
+          <hr/>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
