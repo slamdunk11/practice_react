5bd8b5ac9191a8705a1d7a8aeaed85862d78588c
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-07-15 01:56:15 +0900

    board login 페이지 만들기

---
+++ board/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Text, Input, Grid, Button} from "../elements/index";
+
+const Login = (props) => {
+    return (
+        <React.Fragment>
+            <Grid>
+                
+            </Grid>
+        </React.Fragment>
+    )
+}
