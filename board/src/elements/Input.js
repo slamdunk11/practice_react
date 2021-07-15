566f7d69878bbd99df315ac8247800e079ac26d0
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-07-16 01:33:01 +0900

    Input 컴포넌트 border 제거, 크기 조정

--- board/src/elements/Input.js
+++ board/src/elements/Input.js
@@ -33,13 +33,16 @@ Input.defaultProps = {
 
 const ElInput = styled.input`
     width: 100%;
-  
+    border: none;
+    font-size: 30px;
+    margin: 15px 0px 10px 15px;
     
 `;
 
 const ElTextarea = styled.textarea`
     width: 100%;
-
+    border: none;
+    margin: 5px 0px 0px 15px;
     
 `;
 
