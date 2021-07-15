c95c603e3b984b118d218b46f9ff8fc6e3613ece
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-07-15 17:27:23 +0900

    페이지 이동 버튼 만들기, history 연결

--- board/src/pages/PostList.js
+++ board/src/pages/PostList.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Grid, Button, Image, Text } from "../elements/index";
 import Post from "../components/Post";
-import {useSelector} from "react-redux";
+
+import {useSelector, useDispatch} from "react-redux";
+import {actionCreators as postActions} from "../redux/modules/post";
 
 
 
 const PostList = (props) => {
+    const {history} = props;
+    const dispatch = useDispatch();
     const post_list = useSelector((state) => state.post.list);
+
+    // console.log(post_list);
+
+    React.useEffect(() => {
+        dispatch(postActions.getPostFB());
+        console.log(post_list);
+    }, []);
+    //effect를 수행하고 (mount하거나 unmount할 때) 
+    //그것을 한 번만 실행하고 싶다면 두번째 인자로
+    //[]
     return(
         <React.Fragment>
+           <Grid is_flex width="auto" padding="15px">
+                <Grid>아이디</Grid>
+                <Grid is_flex width="auto">
+                    <Button shape="round" margin="20px" _onClick={()=>{
+                        history.replace("/write");
+                    }}>새 글 작성</Button>
+                    <Image></Image>
+                </Grid>
+            </Grid>
            <Grid>
                 <Grid max_width="800px" margin="0 auto">
                     <Grid>
@@ -24,6 +47,7 @@ const PostList = (props) => {
                             <Button>소개</Button>
                         </Grid>
                         <Grid>
+                            {/* <Post></Post> */}
                             {post_list.map((p, idx) => {
                                 return <Post key={idx} {...p} />
                             })}
--- board/src/pages/Write.js
+++ board/src/pages/Write.js
@@ -3,15 +3,18 @@ import { Grid, Button, Image, Text, Input } from "../elements/index";
 
 
 const Write = (props) => {
+    const {history} = props;
     return(
         <React.Fragment>
-            <Grid>
+            <Grid margin="15px 0px 0px 0px">
                 <Input placeholder="제목을 입력하세요"/>
                 <Input multilines placeholder="당신의 이야기를 적어보세요"/>
             </Grid>
             <Grid is_flex>
                 <Grid width="auto">
-                    <Button>나가기</Button>
+                    <Button _onClick={() => {
+                        history.goBack();
+                    }}>나가기</Button>
                 </Grid>
                 <Grid width="auto">
                     <Button>임시저장</Button>
