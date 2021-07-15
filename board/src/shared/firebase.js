952f49fe63a59bc8b462ddb54cec6f5b5ed15c20
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-07-15 17:26:46 +0900

    firebase 연결

---
+++ board/src/shared/firebase.js
@@ -0,0 +1,28 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+    //firebase 설정과 관련된 개인정보
+    apiKey: "AIzaSyBwSmCG2Z832oGQzJX09S_4cLdvuTJfXGo",
+    authDomain: "board-549d9.firebaseapp.com",
+    projectId: "board-549d9",
+    storageBucket: "board-549d9.appspot.com",
+    messagingSenderId: "480351227840",
+    appId: "1:480351227840:web:3c43b989ddd521b5594225",
+    measurementId: "G-4EVEJ0RLH6"
+}
+
+//firebaseConfig 정보로 firebase 시작
+firebase.initializeApp(firebaseConfig);
+
+//firebase의 firestore 인스턴스를 변수에 저장
+const firestore = firebase.firestore();
+
+const apiKey = firebaseConfig.apiKey;
+const auth = firebase.auth();
+
+
+// 필요한 곳에서 사용할 수 있도록 내보내기
+export {firestore, auth, apiKey};
+
